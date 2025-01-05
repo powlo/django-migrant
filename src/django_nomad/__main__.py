9d8e96445d18b71d50359f349b64927f8e1bf1d8
powlo <paulcbackhouse@gmail.com>
2025-01-05 17:00:13 +0000

    Remove reliance on files that name branch.
    Use the difference between the applied migrations and the disk
    migrations to figure out what needs to be rolled back.

--- src/django_nomad/__main__.py
+++ src/django_nomad/__main__.py
@@ -37,8 +37,6 @@ dump_parser = subparsers.add_parser("dump")
 dump_parser.set_defaults(func=commands.dump)
 
 find_targets_parser = subparsers.add_parser("find_targets")
-find_targets_parser.add_argument("--src", type=str, nargs="?", required=True)
-find_targets_parser.add_argument("--dest", type=str, nargs="?", required=True)
 find_targets_parser.set_defaults(func=commands.find_targets)
 
 install_parser = subparsers.add_parser("install")
--- src/django_nomad/commands.py
+++ src/django_nomad/commands.py
@@ -5,41 +5,31 @@ from pathlib import Path
 
 from django.db import DEFAULT_DB_ALIAS, connections
 from django.db.migrations.loader import MigrationLoader
-from dulwich import porcelain
-from dulwich.repo import Repo
 
 from . import hook_templates
 
 
 def dump(args):
     base_path = Path(".")
-    repo = Repo(".")
-    branch = porcelain.active_branch(repo)
-    filename = base_path / ".nomad" / "branch" / branch.decode() / "nodes.json"
+    filename = base_path / ".nomad" / "nodes.json"
     connection = connections[DEFAULT_DB_ALIAS]
     loader = MigrationLoader(connection)
-    migration_keys = json.dumps(list(loader.disk_migrations.keys()))
+    targets = set(loader.applied_migrations) - set(loader.disk_migrations)
+
+    targets_as_json = json.dumps(list(targets))
     filename.parent.mkdir(exist_ok=True, parents=True)
     with open(filename, "w+") as fh:
-        fh.write(migration_keys)
+        fh.write(targets_as_json)
 
 
 def find_targets(args):
     connection = connections[DEFAULT_DB_ALIAS]
     loader = MigrationLoader(connection)
     base_path = Path(".")
-
-    src_filename = base_path / ".nomad" / "branch" / args.src / "nodes.json"
+    src_filename = base_path / ".nomad" / "nodes.json"
     with open(src_filename) as fh:
-        src_nodes = fh.read()
+        node_names = [tuple(n) for n in json.loads(fh.read())]
 
-    dest_filename = base_path / ".nomad" / "branch" / args.dest / "nodes.json"
-    with open(dest_filename) as fh:
-        dest_nodes = fh.read()
-
-    src_nodes = set([tuple(x) for x in json.loads(src_nodes)])
-    dest_nodes = set([tuple(x) for x in json.loads(dest_nodes)])
-    node_names = src_nodes - dest_nodes
     nodes = [loader.graph.node_map[n] for n in node_names]
 
     targets = set()
