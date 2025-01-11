87d9fe407dfeef2b74fb78cdaaecb39776ef0ea1
powlo <paulcbackhouse@gmail.com>
2025-01-11 16:58:19 +0000

    Restore __main__.py, allowing invocation without app in INSTALLED_APPS

---
+++ src/django_nomad/__main__.py
@@ -0,0 +1,12 @@
+import io
+import sys
+
+import django
+
+from django_nomad.management.commands.nomad import Command
+
+django.setup()
+# Mimic args when the command is called as a django-admin command.
+# Required because commands assume positional values in sys.argv.
+argv = ["django-admin", "nomad"] + sys.argv[1:]
+Command(stdout=io.StringIO(), stderr=io.StringIO()).run_from_argv(argv)
