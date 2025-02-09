65c76081033eef3eb69866473e5fe2f6c14591fb
powlo <paulcbackhouse@gmail.com>
2025-02-09 15:39:02 +0000

    Bug fix
    Output now correctly routed to stdout/stderr by omission of params
    to Command.

--- src/django_nomad/__main__.py
+++ src/django_nomad/__main__.py
@@ -10,4 +10,4 @@ django.setup()
 # Mimic args when the command is called as a django-admin command.
 # Required because commands assume positional values in sys.argv.
 argv = ["django-admin", "nomad"] + sys.argv[1:]
-Command(stdout=io.StringIO(), stderr=io.StringIO()).run_from_argv(argv)
+Command().run_from_argv(argv)
